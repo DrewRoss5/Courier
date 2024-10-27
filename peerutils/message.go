01c26defdcd4f7a778a245083b9a47b7dd708682
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-10-26 18:34:45 -0600

    Fix oversight with message deletion
    This will now delete the last message sent by the user, not the last message sent at all

--- peerutils/message.go
+++ peerutils/message.go
@@ -1,36 +1,279 @@
 package peerutils
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
+	"os"
+	"slices"
+	"strconv"
+	"syscall"
 	"time"
+
+	"github.com/DrewRoss5/courier/cryptoutils"
+	"golang.org/x/term"
 )
 
-const Red = "\033[31m"
-const Green = "\033[32m"
-const Yellow = "\033[33m"
-const Blue = "\033[34m"
-const Magenta = "\033[35m"
-const Cyan = "\033[36m"
-const Gray = "\033[37m"
-const White = "\033[97m"
-const Bold = "\033[1m"
-const Italic = "\033[3m"
-const ColorReset = "\033[0m"
+const (
+	DEFAULT_ROUNDS = 256
+	SALT_SIZE      = 16
+	MAX_MSG_COUNT  = 50
+)
 
-type Message struct {
-	content  string
-	timeSent string
-	sender   *User
+type Chatroom struct {
+	Tunnel   Tunnel
+	Messages map[uint32]Message
+	MaxId    uint32
+	Active   bool
 }
 
-// displays a message to an output stream, usually  this will be stdout, but this needs to be adjustable for archival purposes
-func (m Message) Display(stream io.Writer) {
-	fmt.Fprintf(stream, "%v%v%v%v @ %v%v%v%v: %v", Bold, m.sender.Color, m.sender.Name, ColorReset, Italic, Yellow, m.timeSent, ColorReset, m.content)
+// appends a new message to the chat history
+func (c *Chatroom) pushMessage(msg *string, user *User) {
+	newMessage := NewMessage(*msg, user)
+	c.Messages[c.MaxId] = *newMessage
+	c.MaxId++
+	if c.MaxId == 0xffffffff {
+		c.Messages = make(map[uint32]Message)
+		c.serverMessage("Message limit reached. Chat history cleared")
+	}
 }
 
-// constructs a new message, making a note of the current timestamp
-func NewMessage(content string, usr *User) *Message {
-	timeStr := time.Now().Format("15:04:05")
-	return &Message{content: content, sender: usr, timeSent: timeStr}
+// appends a new message sent from the chatroom itself, used for alerts, and the like
+func (c *Chatroom) serverMessage(msg string) {
+	colored := fmt.Sprintf("%v%v%v\n", Green, msg, ColorReset)
+	c.pushMessage(&colored, &User{Name: "Chatroom", Id: "", Color: Green})
+}
+
+// pushes an error message to chatroom
+func (c *Chatroom) errorMessage(msg string) {
+	colored := fmt.Sprintf("%v%vError:%v %v %v\n", Bold, Red, Yellow, msg, ColorReset)
+	c.pushMessage(&colored, &User{Name: "Chatroom", Id: "", Color: Green})
+}
+
+// awaits an incoming message, and handles it according to its code
+func (c *Chatroom) AwaitMessage() error {
+	if !c.Active {
+		return errors.New("chatroom: this chatroom is no longer Active")
+	}
+	msg, err := c.Tunnel.AwaitMessage()
+	if err != nil {
+		return err
+	}
+	// seperate the message from its code and handle it accordingly
+	msgCode := msg[0]
+	msg = msg[1:]
+	switch msgCode {
+	case MESSAGE_TXT:
+		messageStr := string(msg)
+		c.pushMessage(&messageStr, &c.Tunnel.Peer)
+	case MESSAGE_DISCONNECT:
+		c.Active = false
+	case MESSAGE_DELETE:
+		id := binary.LittleEndian.Uint32(msg)
+		message, ok := c.Messages[id]
+		if ok && message.sender.Id == c.Tunnel.Peer.Id {
+			delete(c.Messages, id)
+		}
+	case CHAT_ARCHIVE:
+		c.serverMessage(fmt.Sprintf("%v%v%v archived this chat.", c.Tunnel.Peer.Color, c.Tunnel.Peer.Name, Green))
+	default:
+		return errors.New("chatroom: invalid message recieved")
+	}
+	return nil
+}
+
+// sends a string message to the peer
+func (c *Chatroom) SendMessage(msg *string) error {
+	tmp := []byte(*msg)
+	msgBytes := append([]byte{MESSAGE_TXT}, tmp...)
+	err := c.Tunnel.SendMessage(msgBytes)
+	if err != nil {
+		return err
+	}
+	c.pushMessage(msg, &c.Tunnel.User)
+	return nil
+
+}
+
+// displays all of the Messages currently in the archive
+func (c Chatroom) DisplayMessages(file io.Writer) {
+	if len(c.Messages) == 0 {
+		fmt.Printf("%v%vNo messages to display%v\n", Italic, Gray, ColorReset)
+		return
+	}
+	var i uint32
+	for i = 0; i < c.MaxId; i++ {
+		// under normal circumstances, all id's should be sequential, however, this is to account for messages potentially being deleted
+		message, ok := c.Messages[i]
+		if ok {
+			message.Display(file)
+		}
+	}
+}
+
+// handles a chat command, and returns the string to be output to the terminal after running
+func (c *Chatroom) HandleCommand(command string, args []string) {
+	switch command {
+	// clears the message history
+	case ">clear":
+		c.Messages = make(map[uint32]Message)
+		c.serverMessage("Messages cleared")
+	// terminates the connection
+	case ">disconnect":
+		err := c.Tunnel.Shutdown()
+		if err != nil {
+			c.errorMessage("failed to close the chatroom")
+			return
+		}
+		c.serverMessage("Chat closed.")
+	// returns the peer's ID
+	case ">peerid":
+		c.serverMessage(fmt.Sprintf("%v%v%v Has the ID\n%v%v%v", c.Tunnel.Peer.Color, c.Tunnel.Peer.Name, ColorReset, Green, c.Tunnel.Peer.Id, ColorReset))
+	// deletes a message from the chat history on both user's ends
+	case ">delete":
+		var id uint32
+		if len(args) == 0 {
+			id = c.MaxId - 1
+			// find the last message the user sent
+			for id > 0 {
+				message, ok := c.Messages[id]
+				if ok && message.sender.Id == c.Tunnel.User.Id {
+					break
+				}
+				id--
+			}
+		} else {
+			tmp, err := strconv.Atoi(args[1])
+			if err != nil {
+				c.errorMessage("Invalid message id")
+			}
+			id = uint32(tmp)
+		}
+		message, ok := c.Messages[id]
+		if !ok || message.sender.Id != c.Tunnel.User.Id {
+			c.errorMessage("Invalid message id")
+			return
+		}
+		delete(c.Messages, id)
+		buf := make([]byte, 4)
+		binary.LittleEndian.AppendUint32(buf, id)
+		c.Tunnel.SendMessage(append([]byte{MESSAGE_DELETE}, buf...))
+	// archive the chat
+	case ">archive":
+		if len(args) < 1 || len(args) > 2 {
+			c.errorMessage("That command takes between one and two arguments")
+			return
+		}
+		// determine the rounds of hashing the user would like to use, DEFAULT=256
+		var rounds int
+		var err error = nil
+		if len(args) == 1 {
+			rounds = DEFAULT_ROUNDS
+		} else {
+			rounds, err = strconv.Atoi(args[1])
+			if err != nil {
+				c.errorMessage("Rounds must be a positive integer")
+				return
+			}
+			if rounds < 0 {
+				c.errorMessage("Rounds must be a positive integer")
+				return
+			}
+		}
+		fmt.Print("Password: ")
+		password, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			c.errorMessage("Failed to read the message")
+			return
+		}
+		fmt.Print("\nConfirm Password: ")
+		confirm, _ := term.ReadPassword(int(syscall.Stdin))
+		if slices.Compare(confirm, password) != 0 {
+			c.errorMessage("Password does not match confirmation. Archive not saved")
+			return
+		}
+		err = c.ArchiveChat(password, args[0], rounds)
+		if err != nil {
+			c.errorMessage(err.Error())
+			return
+		}
+		// inform the other user that the chat has been archived
+		err = c.Tunnel.SendMessage([]byte{CHAT_ARCHIVE})
+		if err != nil {
+			c.Active = false
+			c.errorMessage("connection severed")
+			return
+		}
+		c.serverMessage("Chat archived")
+	// severs the connection and exits the application
+	case ">exit":
+		err := c.Tunnel.Shutdown()
+		if err != nil {
+			c.errorMessage("failed to close the chatroom")
+			return
+		}
+		os.Exit(0)
+	default:
+		c.errorMessage("unrecognized message")
+		return
+	}
+}
+
+// archives a chat and saves it to a file in a given path
+func (c Chatroom) ArchiveChat(password []byte, path string, rounds int) error {
+	// create the path if needed
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		return err
+	}
+	// generate a key
+	salt := cryptoutils.GenNonce()
+	key := cryptoutils.HashKey(password, salt, rounds)
+	// write the chat to a buffer, and encrypt it
+	var buf bytes.Buffer
+	c.DisplayMessages(&buf)
+	ciphertext, err := cryptoutils.AesEncrypt(buf.Bytes(), key)
+	if err != nil {
+		return err
+	}
+	// parse the rounds into a buffer
+	roundBuf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(roundBuf, uint32(rounds))
+	// save the salt and ciphertext to the file
+	contents := append(roundBuf, salt...)
+	contents = append(contents, ciphertext...)
+	fileName := time.Now().Format("/15-04-05.arc")
+	file, err := os.Create(path + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.Write(contents)
+	return nil
+}
+
+// returns a string of a decrypted chat archive
+func DecryptArchive(fileName string, password []byte) (string, error) {
+	// read and parse the file
+	fileContents, err := os.ReadFile(fileName)
+	if err != nil {
+		return "", err
+	}
+	if len(fileContents) < cryptoutils.AES_MIN_CIPHERTEXT_SIZE+SALT_SIZE {
+		return "", errors.New("archive: invalid archive file")
+	}
+	roundsBuf := fileContents[:4]
+	salt := fileContents[4 : SALT_SIZE+4]
+	ciphertext := fileContents[SALT_SIZE+4:]
+	// read the rounds
+	rounds := binary.LittleEndian.Uint32(roundsBuf)
+	// generate the key and attempt to decrypt the archive
+	key := cryptoutils.HashKey(password, salt, int(rounds))
+	archive, err := cryptoutils.AesDecrypt(ciphertext, key)
+	if err != nil {
+		return "", err
+	}
+	return string(archive), nil
 }
